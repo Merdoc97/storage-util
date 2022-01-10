8b2ae9085f91a7a70ac2381239fe5bfff6065dbc
merdoc97 <merdoc97@gmail.com>
2022-01-10 13:29:39 +0200

    initial commit

---
+++ src/main/java/com/example/storageutil/StorageService.java
@@ -0,0 +1,4 @@
+package com.example.storageutil;
+
+public interface StorageService {
+}
---
+++ src/test/java/com/example/storageutil/StorageUtilApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.storageutil;
+
+import org.junit.jupiter.api.Test;
+
+class StorageUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
